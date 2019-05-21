f7e240599c0254f311cb6d5b99bce39cbad112f4
Adrián Pardini <github@tangopardo.com.ar>
2019-05-20 22:57:06 -0300

    connect to Indi, expose current status over rest api

--- cloudwatcher_weather_server/cloudwatcher_weather_server.py
+++ cloudwatcher_weather_server/cloudwatcher_weather_server.py
@@ -13,6 +13,7 @@ from flask import Flask, render_template, g
 from flask.json import jsonify
 from flask_socketio import SocketIO
 
+import PyIndi
 
 from . import api, devices
 
@@ -24,6 +25,96 @@ app.config['SECRET_KEY'] = 'secret!'
 api.register(app)
 
 socketio = SocketIO(app, async_mode='gevent')
+
+
+class IndiWatcher(PyIndi.BaseClient):
+    def __init__(self, device, host='localhost', port=7624):
+        super(IndiWatcher, self).__init__()
+        self.host = host
+        self.port = port
+        self.device = device
+
+        self.setServer(host, port)
+
+    @property
+    def online(self):
+        return self.device.online
+
+    @online.setter
+    def online(self, value):
+        device = self.device
+        if value != device.online:
+            if value:
+                log.info('Device connected')
+                device.online = True
+            else:
+                log.info('Device disconnected')
+                device.online = False
+
+        return value
+
+    def start(self):
+        self.watchDevice(self.device.name)
+        while not self.connectServer():
+            socketio.sleep(1)
+
+    def newDevice(self, device):
+        name = device.getDeviceName()
+        if name == self.device.name:
+            log.info('Device found: {}'.format(name))
+            self.indi_device = device
+
+    def newProperty(self, p):
+        name = p.getName()
+        if name == 'CONNECTION':
+            if p.getState():
+                self.online = True
+            else:
+                self.online = False
+
+        if name == 'serial':
+            self.device.serial_port = p.getText()[0].text
+
+    def removeProperty(self, p):
+        pass
+
+    def newBLOB(self, bp):
+        pass
+
+    def newSwitch(self, svp):
+        if svp.name == 'CONNECTION':
+            if svp[0].s:
+                self.online = True
+            elif svp[1].s:
+                self.online = False
+
+    def newNumber(self, nvp):
+        if nvp.name == 'sensors':
+            for idx in range(nvp.nnp):
+                prop = nvp[idx]
+                setattr(self.device.sensors, prop.name, prop.value)
+
+    def newText(self, tvp):
+        pass
+
+    def newLight(self, lvp):
+        pass
+
+    def newMessage(self, d, m):
+        msg_text = d.messageQueue(m)
+        if 'Disconnected' in msg_text:
+            self.online = False
+        else:
+            self.online = True
+
+    def serverConnected(self):
+        log.info('Connected to INDI Server')
+
+    def serverDisconnected(self, code):
+        log.info('Connection to INDI Server closed')
+        self.online = False
+        self.indi_device = None
+        self.start()
 
 
 def main():
@@ -71,5 +162,7 @@ def main():
         log.setLevel(level=logging.INFO)
 
     device = devices.create(name=args.indi_device)
+    watcher = IndiWatcher(device=device, host=args.indi_host, port=args.indi_port)
+    socketio.start_background_task(watcher.start)
 
     socketio.run(app, host=args.host, port=args.port, use_reloader=False, debug=True, log_output=True)
